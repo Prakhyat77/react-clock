84ca0ede23335c7b4219c1ced339afeb9b55c07f
Prakhyat77 <57028200+Prakhyat77@users.noreply.github.com>
2022-06-09 00:37:30 +0530

    my first commit react-clock website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [time, settime] = useState(new Date().toLocaleTimeString());
+  
+  const [show, setshow] = useState(false);
+
+  setInterval(() => {
+    settime(new Date().toLocaleTimeString());
+  }, 1000);
+
+  const buttonHandler = () => {
+    setshow(!show);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="container">
+        <div className="box1">
+          <h3 className="text-center">
+            {show ? time : "Click Button to show Current Time"}
+          </h3>
+        </div>
+        <br></br>
+        <div className="box2">
+          <button
+            onClick={buttonHandler}
+            className="btn btn-dark click">
+            {show ? 'Hide' : 'Show'}
+          </button>
+        </div>
+      </div>
+  )
 }
 
-export default App;
+export default App
